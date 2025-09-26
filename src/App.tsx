c5eb91573d6a6df21d0e6315fdee8ffa473f6994
CureForCode <artursomov@yahoo.de>
2025-09-26 21:17:28 +0200

    Lesson 18: Redux. Cat fact.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // Lessons
 //import Lesson_16 from "lessons/Lesson_16/Lesson_16"
+import Lesson_18 from "lessons/Lesson_18/Lesson_18"
 
 //Homeworks
-import Homework_16 from "homeworks/Homework_16/Homework_16"
+//import Homework_16 from "homeworks/Homework_16/Homework_16"
+
 
 function App() {
   return (
     <>
       {/* Lessons */}
       {/* <Lesson_16 /> */}
+       <Lesson_18 />
 
       {/* Homeworks */}
-      <Homework_16 />
+      {/* <Homework_16 /> */}
     </>
   )
 }
