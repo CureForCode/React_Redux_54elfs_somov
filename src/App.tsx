6877c5f334950891df0cdd4b96e791fdb9e9f26f
CureForCode <artursomov@yahoo.de>
2025-09-28 13:03:04 +0200

    Homework 18: Redux. Random joke generator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // Lessons
 //import Lesson_16 from "lessons/Lesson_16/Lesson_16"
-import Lesson_18 from "lessons/Lesson_18/Lesson_18"
+//import Lesson_18 from "lessons/Lesson_18/Lesson_18"
+
 
 //Homeworks
 //import Homework_16 from "homeworks/Homework_16/Homework_16"
+import Homework_18 from "homeworks/Homework_18/Homework_18"
 
 
 function App() {
@@ -11,10 +13,11 @@ function App() {
     <>
       {/* Lessons */}
       {/* <Lesson_16 /> */}
-       <Lesson_18 />
+      {/*  <Lesson_18 /> */}
 
       {/* Homeworks */}
       {/* <Homework_16 /> */}
+      <Homework_18 />
     </>
   )
 }
